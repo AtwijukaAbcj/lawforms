cc7f1b18681d751eff5c9486e9e73b0b0164d9b2
AtwijukaAbcj <charles.bravo24@gmiail.com>
2026-03-30 09:43:23 -0400

    Remove emoji

--- family_law/settings.py
+++ family_law/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!&l#_0f@fem+xv*9!hpyioz^@82z5)xssc+uoh155d^veb0xw8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lawforms.solichsystems.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
